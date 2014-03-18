4de0c9a381d2235d399b6578d367c5abf6b9d443
nowty <adrian.j.healey@gmail.com>
2014-03-18 16:03:29 +0100

    template

--- netwObserver/analyse/computation/aggregator.py
+++ netwObserver/analyse/computation/aggregator.py
@@ -20,11 +20,11 @@ def getWismLogByType():
 
 
 def getUserByDot11Protocol():
-	stats = {p:0 for _,p in MobileStation.DOT11_PROTOCOLS}
-	
+	stats = "test" #{p:0 for _,p in MobileStation.DOT11_PROTOCOLS}
+
 	for proto,display in MobileStation.DOT11_PROTOCOLS:
 		stats[display] = MobileStation.objects.filter(dot11protocol__exact=proto).count()
 
-	return MobileStation.DOT11_PROTOCOLS
+	return stats
 
 
