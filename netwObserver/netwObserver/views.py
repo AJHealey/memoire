9c06c36ce2e1e536f5fcc3c096da61521be30593
nowty <adrian.j.healey@gmail.com>
2014-03-28 12:29:39 +0100

    fix experiment

--- netwObserver/netwObserver/views.py
+++ netwObserver/netwObserver/views.py
@@ -10,7 +10,7 @@ def home(request):
 	from gatherer.snmp.getter import getAPIfLoadChannelUtilization,getAPIfLoadRxUtilization,getAPIfLoadTxUtilization,getAPIfLoadNumOfClients,getAPIfPoorSNRClients
 	from gatherer.models import AccessPoint
 	ap = AccessPoint.objects.get(macAddress='b8:38:61:43:91:50')
-	context["test"] = [ap.index,getAPIfLoadRxUtilization(ip='192.168.251.170', ap=ap.index),getAPIfLoadTxUtilization(ip='192.168.251.170', ap='.184.56.97.60.37.128'),getAPIfLoadChannelUtilization(ip='192.168.251.170', ap='.184.56.97.60.37.128'),getAPIfLoadNumOfClients(ip='192.168.251.170', ap='.184.56.97.60.37.128'),getAPIfPoorSNRClients(ip='192.168.251.170', ap='.184.56.97.60.37.128')]
+	context["test"] = [ap.index,getAPIfLoadRxUtilization(ip='192.168.251.170', ap=".184.56.97.67.145.80"),getAPIfLoadTxUtilization(ip='192.168.251.170', ap='.184.56.97.60.37.128'),getAPIfLoadChannelUtilization(ip='192.168.251.170', ap='.184.56.97.60.37.128'),getAPIfLoadNumOfClients(ip='192.168.251.170', ap='.184.56.97.60.37.128'),getAPIfPoorSNRClients(ip='192.168.251.170', ap='.184.56.97.60.37.128')]
 
 	return render(request, "common/home.html", context)
 
