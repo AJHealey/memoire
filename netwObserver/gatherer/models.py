31d377c1bbd3f3437d0e5180c15afebd0d4ad023
nowty <adrian.j.healey@gmail.com>
2014-05-02 10:49:17 +0200

    analysis

--- netwObserver/gatherer/models.py
+++ netwObserver/gatherer/models.py
@@ -29,7 +29,7 @@ class APManage(models.Manager):
 
 class AccessPoint(Device):
 	name = models.CharField(max_length=50, null=True)
-	location = models.CharField(max_length=50, null=True, default)
+	location = models.CharField(max_length=50, null=True)
 
 	numOfClients = models.DecimalField(max_digits=4, decimal_places=0, default=0)
 	numOfPoorSNRClients = models.DecimalField(max_digits=4, decimal_places=0, default=0)
