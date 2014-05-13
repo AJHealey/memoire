03dbd6e330563bae3eee877bf8867cce2bb1314e
nowty <adrian.j.healey@gmail.com>
2014-05-13 15:04:33 +0200

    finito

--- netwObserver/netwObserver/wsgi.py
+++ netwObserver/netwObserver/wsgi.py
@@ -12,12 +12,12 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "netwObserver.settings")
 
 
 ### Start-up code (technique inspired by "http://eldarion.com/blog/2013/02/14/entry-point-hook-django-projects/") ###
-from threading import Thread
-from gatherer.snmp.getter import snmpAPDaemon, snmpMSDaemon, snmpRAPDaemon
-from django.conf import settings
-Thread(target=snmpAPDaemon, kwargs={'laps':settings.SNMPAPLAP}).start()
-Thread(target=snmpMSDaemon, kwargs={'laps':settings.SNMPMSLAP}).start()
-Thread(target=snmpRAPDaemon, kwargs={'laps':settings.SNMPRAPLAP}).start()
+#from threading import Thread
+#from gatherer.snmp.getter import snmpAPDaemon, snmpMSDaemon, snmpRAPDaemon
+#from django.conf import settings
+#Thread(target=snmpAPDaemon, kwargs={'laps':settings.SNMPAPLAP}).start()
+#Thread(target=snmpMSDaemon, kwargs={'laps':settings.SNMPMSLAP}).start()
+#Thread(target=snmpRAPDaemon, kwargs={'laps':settings.SNMPRAPLAP}).start()
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
