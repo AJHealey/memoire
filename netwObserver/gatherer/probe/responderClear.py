815b1004a2d3baa93a38c99900e16b056fa33114
cwayembergh <clement.wayembergh@gmail.com>
2014-05-26 18:21:01 +0200

    sender

--- netwObserver/gatherer/probe/responderClear.py
+++ netwObserver/gatherer/probe/responderClear.py
@@ -27,12 +27,12 @@ def handler(clientsocket):
 	print("[+] Connection established")
 	# Phase 1 : Probe send its identity
 	#identity = int.from_bytes(clientsocket.recv(4),byteorder='little') # identity of the probe
-	identity = clientsocket.recv(18) # identity of the probe (MAC address)
+	identity = clientsocket.recv(1024) # identity of the probe (MAC address)
 	print("[+] Identity received: %s" % identity)
 	clientsocket.send(b'1')
 
 	## Phase 2 = Data receive
-	dataSize = int.from_bytes(clientsocket.recv(4),byteorder='little')
+	dataSize = int.from_bytes(clientsocket.recv(4),byteorder='big')
 	print("[*] Size received (%s)" % dataSize)
 	clientsocket.send(b'1')
 
