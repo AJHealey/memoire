0a6273d1a743fc3b5505fd7fa9c0d482e0e6e3f0
nowty <adrian.j.healey@gmail.com>
2014-05-27 13:45:05 +0200

    fix responder

--- wpa_supplicant/connectClear.c
+++ wpa_supplicant/connectClear.c
@@ -48,7 +48,7 @@ int sendLogs(char *filepath) {
 	read(sockfd, recvBuff, 1);
 
 	// Phase 2 : Data sending
-	int *fd = open(filepath, O_RDONLY);
+	int fd = open(filepath, O_RDONLY);
 	int logsize = lseek(fd,0,SEEK_END);
 	lseek(fd,0,SEEK_SET);
 
